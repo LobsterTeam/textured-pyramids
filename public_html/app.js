bb84d4e4641d337f5bf7457e65e8b93d2841b6d4
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2019-12-27 16:12:33 +0300

    625 triangles

--- public_html/app.js
+++ public_html/app.js
@@ -1,39 +1,60 @@
 "use strict";
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 // FIDAN SAMET 21727666
 // OGUZ BAKIR 21627007
 
+var canvas;
+var gl;
+
+var numPositions = 36;
 
 var positionsArray = [];
+var normalsArray = [];
 var texCoordsArray = [];
-var numOfComponents = 3;        // x, y and z (3d)
-var offset = 0;
-var normalize = false;
-var stride = 0;
-var rotateTheta = 0.0;
-var rotate = true;
-var rotateStep = 1.0;
-var at = vec3(0.0,0.0,0.0);
-var up = vec3(0.0,1.0,0.0);
+
+var lightPosition = vec4(1.0, 1.0, 1.0, 0.0);
+var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0);
+var lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
+var lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
+
+var materialAmbient = vec4(1.0, 1.0, 1.0, 1.0);
+var materialDiffuse = vec4(1.0, 0.8, 0.0, 1.0);
+var materialSpecular = vec4(1.0, 0.8, 0.0, 1.0);
+var materialShininess = 100.0;
+
+var ctm;
+var ambientColor, diffuseColor, specularColor;
+var modelViewMatrix, projectionMatrix;
+var viewerPos;
+var program;
+var xAxis = 0;
+var yAxis = 1;
+var zAxis = 2;
+var axis = 0;
+var r = 0.0 , g = 0.0, b = 0.0;
+var rotateTheta = vec3(0, 0, 0);
+var thetaLoc;
+var flag = false;
+var texture;
+var ambientProduct;
+
+// Camera stuff
+var phi = -9.5;
+var theta = -2.0;
 var eye;
-var phi = 0.0;
-var theta = 0.0;
-var projectionMatrix;
-var modelViewMatrix;
 var near = 0.3;
-var far = 100.0;     // ne kadar far o kadar view volume un icinde
-var radius = 5.0;   // ne kadar radius o kadar uzak
-var fovy = 45.0;    // ne kadar fovy o kadar uzak
-var surfaceVertex = [vec3(-10.0, 0.0, 10.0), vec3(-10.0, 0.0, -10.0), vec3(10.0, 0.0, -10.0), 
-                    vec3(10.0, 0.0, -10.0), vec3(10.0, 0.0, 10.0), vec3(-10.0, 0.0, 10.0)];
-                
-var gl, teapotShader, texture;
-                
+var far = 500.0;
+var radius = 5.0;
+var fovy = 45.0;
+var cameraTranslation = vec3(0.0, 0.0, 0.0);
+var at = vec3(0.0, 0.0, 0.0);
+var up = vec3(0.0, 1.0, 0.0);
+var aspect = 1.0;
+
+
+var i, j;
+
+
 var texCoord = [
     vec2(0, 0),
     vec2(0.5, 1),
@@ -42,239 +63,153 @@ var texCoord = [
 ];
                 
 var vertices = [
-        vec3(-0.5, 0.0, 0.5),
-        vec3(0.0, 1.0, 0.0),
-        vec3(0.5,  0.0, 0.5),
-        vec3(0.5,  0.0,  -0.5),
-        vec3(-0.5,  0.0, -0.5)
-    ];
-var aspect = 1.0;
+    vec4(-0.5, 0.0, 0.5, 1.0),
+    vec4(0.0, 1.0, 0.0, 1.0),
+    vec4(0.5,  0.0, 0.5, 1.0),
+    vec4(0.5,  0.0,  -0.5, 1.0),
+    vec4(-0.5,  0.0, -0.5, 1.0)
+];
 
-var cameraTranslation = vec3(5.0, 3.0, 5.0);
-
-function pyramid(a, b, c) {
-
-     positionsArray.push(vertices[a]);
-     texCoordsArray.push(texCoord[0]);
-     
-     positionsArray.push(vertices[b]);
-     texCoordsArray.push(texCoord[1]);
-     
-     positionsArray.push(vertices[c]);
-     texCoordsArray.push(texCoord[2]);
-     
+function getPyramidVertex (index, i, j) {
+    switch (index) {
+        case 0:
+            return vec4((i*4) + 1, 0.0, (j*4) + 3, 1.0);
+            
+            break;
+        case 1:
+            return vec4((i*4) + 2, 1.0, (j*4) + 2, 1.0);
+            break;
+        case 2:
+            return vec4((i*4) + 1, 0.0, (j*4) + 1, 1.0);
+            
+            
+            break;        
+        case 3:
+            return vec4((i*4) + 3, 0.0, (j*4) + 1, 1.0);
+            
+            break;
+        case 4:
+            return vec4((i*4) + 3, 0.0, (j*4) + 3, 1.0);
+            break;
+    }
 }
 
-function texturedPyramid()
-{
-    pyramid(1, 0, 2);
-    pyramid(1, 2, 3);
-    pyramid(1, 3, 4);
-    pyramid(1, 4, 0);
+function triangle(a, b, c, i, j) {
+    
+    var t1 = subtract(getPyramidVertex(b, i, j),getPyramidVertex(a, i, j));
+    var t2 = subtract(getPyramidVertex(c, i, j), getPyramidVertex(b, i, j));
+    var normal = cross(t1, t2);
+    normal = vec3(normal);
+
+    positionsArray.push(getPyramidVertex(a, i, j));
+    texCoordsArray.push(texCoord[0]);
+    normalsArray.push(normal);
+
+    positionsArray.push(getPyramidVertex(b, i, j));
+    texCoordsArray.push(texCoord[1]);
+    normalsArray.push(normal);
+
+    positionsArray.push(getPyramidVertex(c, i, j));
+    texCoordsArray.push(texCoord[2]);
+    normalsArray.push(normal);
+}
+
+function pyramid() {
+    for (i = 0; i < 25; i++) {
+        for (j = 0; j < 25; j++) {
+            triangle(1, 0, 2, i, j);
+            triangle(1, 2, 3, i, j);
+            triangle(1, 3, 4, i, j);
+            triangle(1, 4, 0, i, j);
+        }
+    }
 }
 
 function configureTexture( image ) {
     texture = gl.createTexture();
     gl.bindTexture( gl.TEXTURE_2D, texture );
-    console.log(image);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
     gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image );
-    //gl.generateMipmap( gl.TEXTURE_2D );
+    gl.generateMipmap( gl.TEXTURE_2D );
     gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_LINEAR );
     gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST );
-    //gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-
-    console.log(teapotShader);
-    gl.uniform1i(gl.getUniformLocation(teapotShader, "texture2d"), 0);
-}
-
-function rafAsync() {
-    return new Promise(resolve => {
-        requestAnimationFrame(resolve); //faster than set time out
-    });
-}
-
-async function getImageFromHtml(id) {
-    const element = document.getElementById(id);
-    while (element === null) {
-        await rafAsync();
-    }
-    return element;
+    gl.uniform1i(gl.getUniformLocation(program, "texture2d"), 0);
 }
 
 
-function main() {
-    const canvas = document.querySelector("#glCanvas");
-    canvas.height = $(window).height();
-    canvas.width = $(window).width();
-    gl = canvas.getContext("webgl2");
-
-    if (!gl) {
-        alert("Unable to initialize WebGL. Your browser or machine may not support it.");
-        return;
-    }
-    
-    
-
-    
-    texturedPyramid();
-    
-    gl.viewport(0, 0, canvas.width, canvas.height);
+window.onload = function init() {
+    canvas = document.getElementById("glCanvas");
     aspect = canvas.width / canvas.height;
-    
-    gl.clearColor(255 / 255.0, 255 / 255.0, 255 / 255.0, 1.0);       // white
-    gl.clearDepth(1.0);
+    gl = canvas.getContext('webgl2');
+    if (!gl) alert( "WebGL 2.0 isn't available");
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
     gl.enable(gl.DEPTH_TEST);
-    gl.depthFunc(gl.LEQUAL);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    var type = gl.FLOAT;
+    //  Load shaders and initialize attribute buffers
+    program = initShaderProgram(gl, vertexShader, teapotFragmentShader);
+    gl.useProgram(program);
+
+    pyramid();
+
+    var nBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, nBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW);
+
+    var normalLoc = gl.getAttribLocation(program, "aNormal");
+    gl.vertexAttribPointer(normalLoc, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(normalLoc);
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(positionsArray), gl.STATIC_DRAW);
+
+    var positionLoc = gl.getAttribLocation(program, "aPosition");
+    gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(positionLoc);
+    
+    var tBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, tBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW );
+    
+    var vTexCoord = gl.getAttribLocation( program, "vTexCoord" );
+    gl.vertexAttribPointer( vTexCoord, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vTexCoord );
+
+    thetaLoc = gl.getUniformLocation(program, "theta");
+
+    viewerPos = vec3(0.0, 0.0, -20.0);
+
+    projectionMatrix = ortho(-1, 1, -1, 1, -100, 100);
     
 
-    teapotShader = initShaderProgram(gl, vertexShader, teapotFragmentShader);
-    gl.useProgram(teapotShader);
-    const teapotBuffer = gl.createBuffer();
-    gl.enableVertexAttribArray(gl.getAttribLocation(teapotShader, 'i_position'));
-    var shaderRotateTheta = gl.getUniformLocation(teapotShader, 'u_rotate_theta');
-    var shaderModelView = gl.getUniformLocation(teapotShader, 'u_model_view');
-    var shaderProjection = gl.getUniformLocation(teapotShader, 'u_projection');
+    var diffuseProduct = mult(lightDiffuse, materialDiffuse);
+    var specularProduct = mult(lightSpecular, materialSpecular);
     
-    const planeShader = initShaderProgram(gl, vertexShader, planeFragmentShader);
-    const planeBuffer = gl.createBuffer();
-    gl.enableVertexAttribArray(gl.getAttribLocation(planeShader, 'i_position'));
-    var planeTheta = gl.getUniformLocation(planeShader, 'u_rotate_theta');
-    var planeModelView = gl.getUniformLocation(planeShader, 'u_model_view');
-    var planeProjection = gl.getUniformLocation(planeShader, 'u_projection');
+    gl.uniform4fv(gl.getUniformLocation(program, "uDiffuseProduct"),
+       diffuseProduct );
+    gl.uniform4fv(gl.getUniformLocation(program, "uSpecularProduct"),
+       specularProduct );
+    gl.uniform4fv(gl.getUniformLocation(program, "uLightPosition"),
+       lightPosition );
 
-    function render () {
-        
-        if (rotate) {
-            rotateTheta += rotateStep;
-        }
-        
-        canvas.height = $(window).height();
-        canvas.width = $(window).width();
+    gl.uniform1f(gl.getUniformLocation(program,
+       "uShininess"), materialShininess);
 
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-        // assign matrices
-        eye = vec3(radius * Math.sin(theta) * Math.cos(phi), 
-                    radius * Math.sin(theta) * Math.sin(phi), radius * Math.cos(theta));
-        eye = add(eye, cameraTranslation);
-        modelViewMatrix = lookAt(eye, at, up);
-        projectionMatrix = perspective(fovy, aspect, near, far);
-        
-        // TEAPOT
-        gl.bindBuffer(gl.ARRAY_BUFFER, teapotBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, flatten(positionsArray), gl.STATIC_DRAW);
-    
-        // TEAPOT POSITIONS
-        gl.vertexAttribPointer(gl.getAttribLocation(teapotShader, 'i_position'),
-            numOfComponents, type, normalize, stride, offset);
-        gl.useProgram(teapotShader);
-        
-        // TEAPOT UNIFORMS
-        gl.uniform1f(shaderRotateTheta, rotateTheta);
-        gl.uniformMatrix4fv(shaderModelView, false, flatten(modelViewMatrix));
-        gl.uniformMatrix4fv(shaderProjection, false, flatten(projectionMatrix));
-        
-        var tBuffer = gl.createBuffer();
-        gl.bindBuffer( gl.ARRAY_BUFFER, tBuffer );
-        gl.bufferData( gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW );
-        
-        var vTexCoord = gl.getAttribLocation( teapotShader, "vTexCoord" );
-        gl.vertexAttribPointer( vTexCoord, 2, gl.FLOAT, false, 0, 0 );
-        gl.enableVertexAttribArray( vTexCoord );
-        
-        // DRAW TEAPOT
-        console.log(positionsArray.length);
-        gl.drawArrays(gl.TRIANGLES, offset, positionsArray.length);
- 
-        // PLANE
-        //gl.bindBuffer(gl.ARRAY_BUFFER, planeBuffer);
-        //gl.bufferData(gl.ARRAY_BUFFER, flatten(surfaceVertex), gl.STATIC_DRAW);
-        
-        // PLANE POSITIONS
-        //gl.vertexAttribPointer(gl.getAttribLocation(planeShader, 'i_position'),
-                //numOfComponents, type, normalize, stride, offset);
-        //gl.useProgram(planeShader);
-
-        // PLANE UNIFORMS
-       //gl.uniform1f(planeTheta, 0.0);
-       // gl.uniformMatrix4fv(planeModelView, false, flatten(modelViewMatrix));
-        //gl.uniformMatrix4fv(planeProjection, false, flatten(projectionMatrix));
-
-        // DRAW PLANE
-       // gl.drawArrays(gl.TRIANGLES, 0, surfaceVertex.length);
-        
-        //if (rotate){
-            requestAnimationFrame(render);
-        //}
-    }
-    
-    //getImageFromHtml("brickTex").then((image) => {
-    //    console.log(image);
-    //    configureTexture(image);
-        
-    //});
-    
+    gl.uniformMatrix4fv( gl.getUniformLocation(program, "uProjectionMatrix"),
+       false, flatten(projectionMatrix));
+       
     var image = document.getElementById("brickTex");
-     render();
-
     configureTexture( image );
+    render();
     
-
-    document.addEventListener('keydown', function(event) {
+     document.addEventListener('keydown', function(event) {
     
         switch(event.keyCode) {
-            // + key
-            case 107:
-                rotateStep += 1.0;
-                //if (rotate == false) {
-                 //   rotate = true;
-                //    render();
-                //}
+            // O key
+            case 79:
                 break;
-            // - key
-            case 109:
-                rotateStep -= 1.0;
-                //if (rotate == false) {
-                //    rotate = true;
-                //    render();
-                //}
-                break;
-            // up arrow
-            case 38:
-                console.log("up arrow");
-                at[2] -= 0.1;
-                cameraTranslation[2] -= 0.1;
-                break;
-            // down arrow
-            case 40:
-                console.log("down arrow");
-                at[2] += 0.1;
-                cameraTranslation[2] += 0.1;
-                break;
-            // right arrow
-            case 39:
-                console.log("right arrow");
-                at[0] += 0.1;
-                cameraTranslation[0] += 0.1;
-                break;
-            // left arrow
-            case 37:
-                console.log("left arrow");
-                at[0] -= 0.1;
-                cameraTranslation[0] -= 0.1;
-                break;
-            // page up key
-            case 33:
-                console.log("page up");
-                at[1] += 0.1;
-                cameraTranslation[1] += 0.1;
-                break;
-            // page down key
-            case 34:
-                console.log("page down");
-                at[1] -= 0.1;
-                cameraTranslation[1] -= 0.1;
+            // P key
+            case 80:
                 break;
             // e key
             case 69: 
@@ -306,5 +241,32 @@ function main() {
     }
 }
 
+function render(){
 
-main();
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    if(flag) rotateTheta[axis] += 2.0;
+
+    modelViewMatrix = mat4();
+    modelViewMatrix = mult(modelViewMatrix, rotate(rotateTheta[xAxis], vec3(1, 0, 0)));
+    modelViewMatrix = mult(modelViewMatrix, rotate(rotateTheta[yAxis], vec3(0, 1, 0)));
+    modelViewMatrix = mult(modelViewMatrix, rotate(rotateTheta[zAxis], vec3(0, 0, 1)));
+    ambientProduct = mult(lightAmbient, materialAmbient);
+    
+    eye = vec3(radius * Math.sin(theta) * Math.cos(phi), 
+            radius * Math.sin(theta) * Math.sin(phi), radius * Math.cos(theta));
+    eye = add(eye, cameraTranslation);
+    modelViewMatrix = lookAt(eye, at, up);
+    projectionMatrix = perspective(fovy, aspect, near, far);
+    
+    gl.uniformMatrix4fv(gl.getUniformLocation(program,
+            "uModelViewMatrix"), false, flatten(modelViewMatrix));
+            
+    gl.uniform4fv(gl.getUniformLocation(program, "uAmbientProduct"),
+       ambientProduct);
+
+       console.log(positionsArray.length);
+    gl.drawArrays(gl.TRIANGLES, 0, positionsArray.length);
+
+    requestAnimationFrame(render);
+}
--- public_html/shader.js
+++ public_html/shader.js
@@ -6,23 +6,57 @@
 
 const vertexShader = `#version 300 es
 
-    in vec3 i_position;
-    in vec2 vTexCoord;
-    uniform float u_rotate_theta;
-    uniform mat4 u_model_view;
-    uniform mat4 u_projection;
+    in  vec4 aPosition;
+    in  vec3 aNormal;
+    in  vec2 vTexCoord;
 
+    out vec4 vColor;
     out vec2 fTexCoord;
 
-    void main() {
-        float rotate_radian = radians(u_rotate_theta);
-        float rotate_sin = sin(rotate_radian);
-        float rotate_cos = cos(rotate_radian);
-        mat4 ry = mat4(rotate_cos, 0.0, -rotate_sin, 0.0,
-                        0.0, 1.0, 0.0, 0.0,
-                        rotate_sin, 0.0, rotate_cos, 0.0,
-                        0.0, 0.0, 0.0, 1.0);
-        gl_Position = u_projection * u_model_view * vec4(i_position, 1.0);
+    uniform vec4 uAmbientProduct, uDiffuseProduct, uSpecularProduct;
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
+    uniform vec4 uLightPosition;
+    uniform float uShininess;
+
+    void main()
+    {
+
+
+        vec3 pos = -(uModelViewMatrix * aPosition).xyz;
+
+        //fixed light postion
+
+        vec3 light = uLightPosition.xyz;
+        vec3 L = normalize(light - pos);
+
+
+        vec3 E = normalize(-pos);
+        vec3 H = normalize(L + E);
+
+        vec4 NN = vec4(aNormal,0);
+
+        // Transform vertex normal into eye coordinates
+
+        vec3 N = normalize((uModelViewMatrix*NN).xyz);
+
+        // Compute terms in the illumination equation
+        vec4 ambient = uAmbientProduct;
+
+        float Kd = max(dot(L, N), 0.0);
+        vec4  diffuse = Kd*uDiffuseProduct;
+
+        float Ks = pow( max(dot(N, H), 0.0), uShininess );
+        vec4  specular = Ks * uSpecularProduct;
+
+        if( dot(L, N) < 0.0 ) {
+              specular = vec4(0.0, 0.0, 0.0, 1.0);
+        }
+
+        gl_Position = uProjectionMatrix * uModelViewMatrix *aPosition;
+        vColor = ambient + diffuse +specular;
+
+        vColor.a = 1.0;
         fTexCoord = vTexCoord;
     }
 `;
@@ -30,14 +64,20 @@ const vertexShader = `#version 300 es
 const teapotFragmentShader = `#version 300 es
 
     precision mediump float;
-    in vec2 fTexCoord;
 
-    uniform sampler2D texture2d;
-    out vec4 o_color;
-    
-    void main() {
-        o_color = vec4(170.0 / 255.0, 178.0 / 255.0, 167.0 / 255.0, 1.0) * texture(texture2d, fTexCoord);
-    }
+
+in vec4 vColor;
+in vec2 fTexCoord;
+
+uniform sampler2D texture2d;
+
+out vec4 fColor;
+
+void
+main()
+{
+    fColor = vec4(255.0 / 255.0, 255.0 / 255.0, 255.0 / 255.0, 1.0) * texture(texture2d, fTexCoord);
+}
 `;
 
 const planeFragmentShader = `#version 300 es
@@ -46,6 +86,6 @@ const planeFragmentShader = `#version 300 es
     out vec4 o_color;
     
     void main() {
-        o_color = vec4(0.0 / 255.0, 255.0 / 255.0, 0.0 / 255.0, 1.0);
+        o_color = vec4(0.0 / 255.0, 0.0 / 255.0, 0.0 / 255.0, 1.0);
     }
 `;
